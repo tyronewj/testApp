595d588e80230ead1f7e34625efa50951f86b6ec
wj <tyronewj@hotmail.com>
2015-05-14 10:21:33 +0800

    增加int ab；

---
+++ app/src/main/java/test1/wj/cn/myapplication/Test.java
@@ -0,0 +1,15 @@
+package test1.wj.cn.myapplication;
+
+/**
+ * -----------------------------------------------
+ * 作者：by TyroneSean
+ * 日期: 2015-05-14
+ * 时间: 10:14
+ * 邮箱：tyronewj@hotmail.com
+ * 功能能描述：
+ * -----------------------------------------------
+ */
+public class Test {
+
+	public int ab;
+}
